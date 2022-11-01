23152395ec3afd117bd53369233fed9d2eac819d
Izamara Agata <izamaraa13contas@gmail.com>
2022-11-01 16:30:56 -0300

    fix:mudança atributo descricao(null =True)

--- produtos/models.py
+++ produtos/models.py
@@ -4,10 +4,10 @@ import uuid
 # Create your models here.
 class Produto(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    categoria_id = models.ForeignKey(
-        "categorias.Categoria", on_delete=models.CASCADE, related_name="produtos"
-    )
+    # categoria_id = models.ForeignKey(
+    #     "categorias.Categoria", on_delete=models.CASCADE, related_name="produtos"
+    # )
     preco = models.DecimalField(max_digits=5, decimal_places=2)
     nome = models.CharField(max_length=100)
     imagem = models.TextField()
-    descricao = models.TextField()
+    descricao = models.TextField(null=True)
