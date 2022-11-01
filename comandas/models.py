3be8549c8d105930f84620b7f2cbc68ffef16cb0
Hitalo <hitalo.2014.hs@gmail.com>
2022-11-01 12:08:45 -0300

    feat: criacao de models da comanda

--- comandas/models.py
+++ comandas/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class ComandaStatus(models.TextChoices):
+    ABERTA = "aberta"
+    FECHADA = "fechada"
+    EM_ROTA_DE_ENTREGA = "em rota de entrega"
+    ENTREGUE = "entregue"
+
+
+class Comanda(models.Model):
+    status = models.CharField(
+        max_length=30, choices=ComandaStatus, default=ComandaStatus.ABERTA
+    )
+    data_criacao = models.DateField(auto_now_add=True)
+
+    conta = models.ForeignKey(
+        "contas.Conta", on_delete=models.CASCADE, references="comandas"
+    )
+
+
+class ComandaProduto(models.Model):
+    quantidade = models.PositiveIntegerField()
+
+    comanda = models.ForeignKey(
+        "comandas.Comanda", on_delete=models.CASCADE, references="comanda_produtos"
+    )
+    produto = models.ForeignKey(
+        "produtos.Produto", on_delete=models.CASCADE, references="comanda_produtos"
+    )
