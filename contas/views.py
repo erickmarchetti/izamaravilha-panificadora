2c91e913084886829da6092f4bb0ad7f37cb3152
Bruno Tiberio <quentemente@gmail.com>
2022-11-01 15:53:55 -0300

    feat: post login e create

--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from contas.models import Conta
+from .serializers import ContaClienteSerializer, ContaFuncionarioSerializer
 
 # Create your views here.
+
+class CriarContasClientView(generics.CreateAPIView):
+
+    queryset = Conta.objects.all()
+    serializer_class = ContaClienteSerializer
+
+
+class CriarContasFuncionarioView(generics.CreateAPIView):
+
+    queryset = Conta.objects.all()
+    serializer_class = ContaFuncionarioSerializer
