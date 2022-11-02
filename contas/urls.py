3b4d5c2894aed076cf30c8735ceb39f82cede47a
Bruno Tiberio <quentemente@gmail.com>
2022-11-02 13:51:33 -0300

    feat: novos endpoints

--- contas/urls.py
+++ contas/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('login/', obtain_auth_token, name='user-login'),
     path('usuario/', views.CriarContasClientView.as_view(), name='user-create-client'),
-    path('funcionario/', views.CriarContasFuncionarioView.as_view(), name='user-create-employee'),    
+    path('funcionario/', views.CriarContasFuncionarioView.as_view(), name='user-create-employee'),
+    path('usuario/contas/', views.ListarTodasContasApenasAdminView.as_view(), name='user-list-clients'),
+    path('usuario/<str:usuario_id>/', views.ListarDeletarPropriaContaApenasAdminOuProprioView.as_view(), name='user-list-delete-self-client'),
+    path('usuario/<str:usuario_id>/atualizar', views.AtualizarPropriaContaView.as_view(), name='user-update-self-client'),  
 ]
