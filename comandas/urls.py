7d825de88a8b12d6d7e391e646dd8c92f8db45f8
ErickMarchetti <erickgmarchetti@gmail.com>
2022-11-02 15:35:26 -0300

    feat: reorganizacao de url

--- comandas/urls.py
+++ comandas/urls.py
@@ -1,18 +1,16 @@
+from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    "comanda/",
-    views.ComandaCriarView.as_view(),
-    "comanda/comanda_id/produto/produto_id/",
-    views.ComandaEditarApagarProdutoView.as_view(),
-    "comanda/comanda_id/status/",
-    views.ComandaEdicaoStatus.as_view(),
-    "comanda/finalizadas/",
-    views.ComandaListarComandasFinalizadas.as_view(),
-    "comanda/abertas/",
-    views.ComandaListarComandasAbertas.as_view(),
-    "comanda/user/",
-    views.ComandaListarTodasAsComandas.as_view(),
-    "comanda/comanda_id/",
-    views.ComandaEspecifica.as_view(),
+    path("comanda/", views.ComandaAdicionarProdutoView.as_view()),
+    path(
+        "comanda/<comanda_id>/produto/<produto_id>/",
+        views.ComandaEditarApagarProdutoView.as_view(),
+    ),
+    path("comanda/<comanda_id>/status/", views.ComandaEdicaoStatus.as_view()),
+    path("comanda/finalizadas/", views.ComandaListarComandasFinalizadas.as_view()),
+    path("comanda/abertas/", views.ComandaListarComandasAbertas.as_view()),
+    path("comanda/user/", views.ComandaListarTodasAsComandas.as_view()),
+    path("comanda/<comanda_id>/", views.ComandaEspecifica.as_view()),
 ]
