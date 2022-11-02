7160d1d0e0fb0e0b20a3a311b355fbbc913487bb
ErickMarchetti <erickgmarchetti@gmail.com>
2022-11-02 15:43:21 -0300

    feat: crud inicial de comanda

--- comandas/views.py
+++ comandas/views.py
@@ -1,40 +1,56 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 from comandas.models import Comanda, Comanda_Produto, ComandaStatus
+from comandas.serializers import (
+    AdicionarOuListarProdutoSerializer,
+    EditarQuantidadeProdutoSerializer,
+)
+from comandas.permissions import ApenasDonoDaComanda
 
-from comandas.serializers import ComandaProdutoSerializer, ComandaSerializer
 
+class ComandaAdicionarProdutoView(generics.CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
-class ComandaCriarView(generics.CreateAPIView):
-    queryset = Comanda.objects
-    serializer_class = ComandaSerializer
+    queryset = Comanda.objects.all()
+    serializer_class = AdicionarOuListarProdutoSerializer
 
     def perform_create(self, serializer):
-        return super().perform_create(serializer)
+        serializer.save(conta=self.request.user)
 
 
 class ComandaEditarApagarProdutoView(generics.UpdateAPIView, generics.DestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [ApenasDonoDaComanda]
+
     queryset = Comanda.objects.all()
-    serializer_class = Comanda
+    serializer_class = EditarQuantidadeProdutoSerializer
 
     def get_object(self):
         comanda_id = self.kwargs["comanda_id"]
         produto_id = self.kwargs["produto_id"]
 
-        return get_object_or_404(
+        comanda_produto = get_object_or_404(
             Comanda_Produto, comanda_id=comanda_id, produto_id=produto_id
         )
+
+        self.check_object_permissions(self.request, comanda_produto)
+
+        return comanda_produto
 
 
 class ComandaEdicaoStatus(generics.UpdateAPIView):
     queryset = Comanda.objects.all()
-    serializer_class = ComandaSerializer
+    serializer_class = ...
     lookup_url_kwarg = "comanda_id"
 
 
 class ComandaListarComandasFinalizadas(generics.ListAPIView):
     queryset = Comanda.objects.all()
-    serializer_class = ComandaSerializer
+    serializer_class = ...
 
     def get_queryset(self):
         return self.queryset.filter(status=ComandaStatus.FECHADA)
@@ -42,18 +58,24 @@ class ComandaListarComandasFinalizadas(generics.ListAPIView):
 
 class ComandaListarComandasAbertas(generics.ListAPIView):
     queryset = Comanda.objects.all()
-    serializer_class = ComandaSerializer
+    serializer_class = ...
 
     def get_queryset(self):
         return self.queryset.filter(status=ComandaStatus.ABERTA)
 
 
 class ComandaListarTodasAsComandas(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Comanda.objects.all()
-    serializer_class = ComandaSerializer
+    serializer_class = AdicionarOuListarProdutoSerializer
+
+    def get_queryset(self):
+        return self.request.user.comandas.all()
 
 
 class ComandaEspecifica(generics.RetrieveAPIView):
     queryset = Comanda.objects.all()
-    serializer_class = ComandaSerializer
+    serializer_class = ...
     lookup_url_kwarg = "comanda_id"
