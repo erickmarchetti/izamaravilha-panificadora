034a15eb6487ddfb2c81e8402287d1454cd38b92
Bruno Tiberio <quentemente@gmail.com>
2022-11-02 13:51:56 -0300

    fix: uuid adicionado

--- contas/models.py
+++ contas/models.py
@@ -1,9 +1,12 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
 class Conta(AbstractUser):
 
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     username = models.CharField(max_length=30, unique=True)
     first_name = models.CharField(max_length=90)
     last_name = models.CharField(max_length=120)
@@ -12,9 +15,6 @@ class Conta(AbstractUser):
     cpf = models.CharField(max_length=11, unique=True)
     telefone = models.CharField(max_length=15)
     pontos_de_fidelidade = models.IntegerField(default=0)
-
-    # endereco = models.OneToOneField('enderecos.Endereco', on_delete=models.CASCADE, related_name='conta')
-
-
+    
 
     REQUIRED_FIELDS = ['first_name', 'last_name', 'data_nascimento', 'cpf', 'telefone',]
