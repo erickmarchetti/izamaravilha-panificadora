3743461ef564599cc5f78219298b924388d2b649
Andryws <andryws.kenzie@gmail.com>
2022-11-03 11:16:32 -0300

    feat: permissoes de categoria add

--- categorias/views.py
+++ categorias/views.py
@@ -1,13 +1,20 @@
 from rest_framework import generics
 from .models import Categoria
 from categorias.serializer import SerializerCategoria
+from rest_framework.authentication import TokenAuthentication
+from .permissoesCategoria import PermissaoCriarCategoria, PermissaoAtualizarCategoria
 
 
 class CategoriasListarOuCriar(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [PermissaoCriarCategoria]
+
     serializer_class = SerializerCategoria
     queryset = Categoria.objects.all()
 
 
 class CategoriasPegarOuAtualizar(generics.RetrieveUpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [PermissaoAtualizarCategoria]
     serializer_class = SerializerCategoria
     queryset = Categoria.objects.all()
