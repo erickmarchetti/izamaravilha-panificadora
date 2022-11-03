926e6f84e23619aecb36f3e07778c8fabc961d4c
Izamara Agata <izamaraa13contas@gmail.com>
2022-11-03 09:28:45 -0300

    feat:criacao de serializer

---
+++ produtos/serializers.py
@@ -0,0 +1,19 @@
+from asyncore import read
+from dataclasses import fields
+from rest_framework import serializers
+
+from produtos.models import Produto
+
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produto
+        fields = [
+            "id",
+            "categoria_id",
+            "preco",
+            "nome",
+            "imagem",
+            "descricao",
+        ]
+        read_only_fields = ["id"]
