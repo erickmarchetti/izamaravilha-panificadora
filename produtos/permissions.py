2117bb311499854dbd151597cfc3c16a8b30d0ad
Izamara Agata <izamaraa13contas@gmail.com>
2022-11-03 10:42:55 -0300

    fix:correcao de permissions

--- produtos/permissions.py
+++ produtos/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class VendedorOuAdminPermissions(permissions.Permission):
+class VendedorOuAdminPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
