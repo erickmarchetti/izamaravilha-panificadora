89dece43f23d7ade1519bb7af7ed4071677fa127
Bruno Tiberio <quentemente@gmail.com>
2022-11-04 18:42:41 -0300

    feat: configuração para envio de email

--- _project/settings.py
+++ _project/settings.py
@@ -164,7 +164,7 @@ AUTH_USER_MODEL = "contas.Conta"
 
 environ.Env.read_env(BASE_DIR / '.env')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = env('EMAIL_PORT')
