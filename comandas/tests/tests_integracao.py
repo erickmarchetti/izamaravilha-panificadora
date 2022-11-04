585a0fcb747776003e5f19d2a106f811d5fb79c0
Hitalo <hitalo.2014.hs@gmail.com>
2022-11-04 14:04:11 -0300

    fix: testando os testes

--- comandas/tests/tests_integracao.py
+++ comandas/tests/tests_integracao.py
@@ -1,3 +1,4 @@
+import json
 import ipdb
 
 from rest_framework.test import APITestCase
@@ -19,33 +20,34 @@ class TesteIntegracaoComanda(APITestCase):
 
         self.superuser = Conta.objects.create_superuser(**usuario_superuser)
 
-        # admin_login = {"username": "jorge", "password": "1234"}
-        # token_admin = self.client.post("/api/login/", data=admin_login)
-        # self.client.credentials(
-        #     HTTP_AUTHORIZATION="Token " + token_admin.json()["token"]
-        # )
-        token_admin = Token.objects.create(user=self.superuser)
-        self.client.credentials(HTTP_AUTHORIZATION="Token " + token_admin.key)
-        self.funcionario = self.client.post("/api/usuario/", data=usuario_funcionario)
+        admin_login = {"username": "jorge", "password": "1234"}
+        token_admin = self.client.post("/api/login/", data=admin_login)
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + token_admin.json()["token"]
+        )
 
+        self.funcionario = self.client.post(
+            "/api/usuario/", data=json.dumps(usuario_funcionario)
+        )
+
+        ipdb.set_trace()
         # self.funcionario = Conta.objects.create_user(**usuario_funcionario)
 
         # self.comum = Conta.objects.create_user(**usuario_comum)
 
-        # produto_1_valores = {
-        #     "preco": 9.00,
-        #     "nome": "Requeijão Cremoso",
-        #     "categoria": Categoria.objects.create(**{"name": "laticínios"}),
-        #     "imagem": "requeijaocremoso.jpg",
-        #     "descricao": "Saboroso e ótimo para acompanhar com pães fresquinhos",
-        # }
+        produto_1_valores = {
+            "preco": 9.00,
+            "nome": "Requeijão Cremoso",
+            "categoria": Categoria.objects.create(**{"name": "laticínios"}),
+            "imagem": "requeijaocremoso.jpg",
+            "descricao": "Saboroso e ótimo para acompanhar com pães fresquinhos",
+        }
         """
         PRODUTO TEM QUE SER CRIADO, JUNTO COM O PRODUTO VEM A CATEGORIA.
         """
-        # self.produto_1 = Produto.objects.create(**produto_1_valores)
+        self.produto_1 = Produto.objects.create(**produto_1_valores)
 
     def test_tentando_criar_uma_comanda(self):
-        ipdb.set_trace()
         token = Token.objects.create(self.comum)
         self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         valores = {
@@ -61,3 +63,57 @@ class TesteIntegracaoComanda(APITestCase):
         # self.assertIn(response.data, "data_criacao")
         # self.assertIn(response.data, "conta")
         # self.assertIn(response.data, "produtos")
+
+
+# class TesteIntegracaoComanda(APITestCase):
+#     @classmethod
+#     def setUpTestData(cls) -> None:
+#         return super().setUpTestData()
+
+#     def setUp(self) -> None:
+
+#         self.superuser = Conta.objects.create_superuser(**usuario_superuser)
+
+#         admin_login = {"username": "jorge", "password": "1234"}
+#         token_admin = self.client.post("/api/login/", data=admin_login)
+#         self.client.credentials(
+#             HTTP_AUTHORIZATION="Token " + token_admin.json()["token"]
+#         )
+
+#         self.funcionario = self.client.post(
+#             "/api/usuario/", data=json.dumps(usuario_funcionario)
+#         )
+
+#         ipdb.set_trace()
+#         # self.funcionario = Conta.objects.create_user(**usuario_funcionario)
+
+#         # self.comum = Conta.objects.create_user(**usuario_comum)
+
+#         produto_1_valores = {
+#             "preco": 9.00,
+#             "nome": "Requeijão Cremoso",
+#             "categoria": Categoria.objects.create(**{"name": "laticínios"}),
+#             "imagem": "requeijaocremoso.jpg",
+#             "descricao": "Saboroso e ótimo para acompanhar com pães fresquinhos",
+#         }
+#         """
+#         PRODUTO TEM QUE SER CRIADO, JUNTO COM O PRODUTO VEM A CATEGORIA.
+#         """
+#         self.produto_1 = Produto.objects.create(**produto_1_valores)
+
+#     def test_tentando_criar_uma_comanda(self):
+#         token = Token.objects.create(self.comum)
+#         self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+#         valores = {
+#             "produto_id": self.produto_1,
+#             "quantidade": 2,
+#         }
+#         response = self.client.post("/api/comanda/", data=valores)
+#         expected_status = status.HTTP_201_CREATED
+
+#         self.assertEqual(expected_status, response.status_code)
+#         # self.assertIn(response.data, "id")
+#         # self.assertIn(response.data, "status")
+#         # self.assertIn(response.data, "data_criacao")
+#         # self.assertIn(response.data, "conta")
+#         # self.assertIn(response.data, "produtos")
